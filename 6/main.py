02a23a4ee5af9059bcb845eb7f8229cf09ad9faa
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-06 22:57:13 +0100

    6xt day

--- 6/main.py
+++ 6/main.py
@@ -58,6 +58,56 @@ def simulate_patrol(grid, start_pos):
     return len(visited)
 
 
+def simulate_guard(grid, start_pos):
+    steps = set()
+    direction = 0
+    current_pos = (start_pos[1], start_pos[0])  
+
+    while True:
+        if direction == 0:  # North
+            next_pos = (current_pos[0] - 1, current_pos[1])
+        elif direction == 1:  # East
+            next_pos = (current_pos[0], current_pos[1] + 1)
+        elif direction == 2:  # South
+            next_pos = (current_pos[0] + 1, current_pos[1])
+        else:  # West
+            next_pos = (current_pos[0], current_pos[1] - 1)
+        
+        if (next_pos[0] < 0 or next_pos[1] < 0 or 
+            next_pos[0] >= len(grid) or next_pos[1] >= len(grid[0])):
+            break
+
+        if grid[next_pos[0]][next_pos[1]] == '#':
+            direction = (direction + 1) % 4
+        elif current_pos + next_pos in steps:
+            return True
+        else:
+            steps.add(current_pos + next_pos)
+            current_pos = next_pos
+
+    return False
+
+
+def count_loop_obstructions(grid, start_pos):
+    loop_count = 0
+    start_pos = (start_pos[1], start_pos[0])
+    
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] != '.':
+                continue
+                
+            original_val = grid[y][x]
+            grid[y][x] = '#'
+            
+            if simulate_guard(grid, start_pos):
+                loop_count += 1
+                
+            grid[y][x] = original_val
+    
+    return loop_count
+
+
 def read_input(filename):
     with open(filename, "r") as file:
         content = file.read()
@@ -68,8 +118,8 @@ def read_input(filename):
 def main():
     content = read_input("input.txt")
     grid, start_pos = parse_map(content)
-    result = simulate_patrol(grid, start_pos)
-    print(f"The guard will visit {result} distinct positions.")
+    print("Distinct positions visited by the guard:", simulate_patrol(grid, start_pos))
+    print("Number of positions for new obstructions to cause loops:", count_loop_obstructions(grid, start_pos))
 
 
 if __name__ == "__main__":
